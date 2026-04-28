74d31ad7a6c9c4e8db04f67326dc88238fb16777
kns3-3 <toma66kfc@docomo.ne.jp>
2026-04-28 16:15:49 +0900

    パスワード認証機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :trips, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_trips, through: :favorites, source: :trip
