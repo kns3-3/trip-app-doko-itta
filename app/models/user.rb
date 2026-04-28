dd0446cec4530e498fe97e067cbf8ff124b349a7
kns3-3 <toma66kfc@docomo.ne.jp>
2026-04-28 15:45:44 +0900

    モデルにアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :trips, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_trips, through: :favorites, source: :trip
 end
